8a063c924762f439798a08a7f6a7031c67ca9208
yvbbrjdr <xyyqzy@hotmail.com>
2021-04-18 14:27:30 -0700

    translation: Update ts

--- LANDrop/locales/LANDrop.zh_CN.ts
+++ LANDrop/locales/LANDrop.zh_CN.ts
@@ -134,27 +134,27 @@ Would you like to receive it?</source>
 <context>
     <name>FileTransferReceiver</name>
     <message>
-        <location filename="../filetransferreceiver.cpp" line="50"/>
+        <location filename="../filetransferreceiver.cpp" line="54"/>
         <source>Cannot create download path: </source>
         <translation>无法创建下载路径： </translation>
     </message>
     <message>
-        <location filename="../filetransferreceiver.cpp" line="54"/>
+        <location filename="../filetransferreceiver.cpp" line="58"/>
         <source>Download path is not writable: </source>
         <translation>下载路径不可写： </translation>
     </message>
     <message>
-        <location filename="../filetransferreceiver.cpp" line="152"/>
+        <location filename="../filetransferreceiver.cpp" line="151"/>
         <source>Unable to open file %1.</source>
         <translation>无法打开文件 %1。</translation>
     </message>
     <message>
-        <location filename="../filetransferreceiver.cpp" line="156"/>
+        <location filename="../filetransferreceiver.cpp" line="155"/>
         <source>Receiving file %1...</source>
         <translation>正在接收文件 %1...</translation>
     </message>
     <message>
-        <location filename="../filetransferreceiver.cpp" line="168"/>
+        <location filename="../filetransferreceiver.cpp" line="167"/>
         <source>Done!</source>
         <translation>完成！</translation>
     </message>
